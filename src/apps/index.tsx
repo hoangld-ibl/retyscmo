1655e06af1872c1b54057826c2d78f065a47d312
hoangld <hoangld@cardano-labo.com>
2019-05-18 23:06:43 +0700

    add welcome page in apps folder.

---
+++ src/apps/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class App extends React.Component {
+    public render() {
+        return (
+            <div> Wellcome!</div>
+        );
+    }
+}
+
+export default App;
